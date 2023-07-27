1fc81ef55f08602e5653d00afd361343374ea21b
sgf <1429709314@qq.com>
2023-07-27 15:27:17 +0800

    Site updated: 2023-07-27 15:27:07

---
+++ js/runtime.js
@@ -0,0 +1,36 @@
+setInterval(() => {
+  let create_time = Math.round(new Date('2023-07-25 00:00:00').getTime() / 1000);                   //在此行修改建站时间
+  let timestamp = Math.round((new Date().getTime()) / 1000);
+  let second = timestamp - create_time;
+  let time = new Array(0, 0, 0, 0, 0);
+
+  var nol = function(h){
+    return h>9?h:'0'+h;
+  }
+  if (second >= 365 * 24 * 3600) {
+    time[0] = parseInt(second / (365 * 24 * 3600));
+    second %= 365 * 24 * 3600;
+  }
+  if (second >= 24 * 3600) {
+    time[1] = parseInt(second / (24 * 3600));
+    second %= 24 * 3600;
+  }
+  if (second >= 3600) {
+    time[2] = nol(parseInt(second / 3600));
+    second %= 3600;
+  }
+  if (second >= 60) {
+    time[3] = nol(parseInt(second / 60));
+    second %= 60;
+  }
+  if (second > 0) {
+    time[4] = nol(second);
+  }
+  if ((Number(time[2])<22) && (Number(time[2])>7)){
+    currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/shen-%E5%B9%B3%E7%A8%B3%E9%99%8D%E8%90%BD-brightgreen?style=social&logo=androidauto&logoColor=blue' title='即将平稳着陆，感谢您这一路的支持哦~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+  }
+  else{
+    currentTimeHtml ="<img class='boardsign' src='https://img.shields.io/badge/shen-%E5%87%86%E5%A4%87%E8%B5%B7%E9%A3%9E-brightgreen?style=social&logo=androidauto&logoColor=blue ' title='飞行之旅即将开启，请系好您的安全带~'><div id='runtime'>" + time[0] + ' YEAR ' + time[1] + ' DAYS ' + time[2] + ' : ' + time[3] + ' : ' + time[4] + '</div>';
+  }
+  document.getElementById("workboard").innerHTML = currentTimeHtml;
+}, 1000);
